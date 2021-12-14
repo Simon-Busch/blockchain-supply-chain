d24f2416d1088ce6cfee938019a2fad666693425
Simon Busch <simon.busch@live.fr>
2021-12-14 08:51:48 +0100

    fix app js basic logic with ether js

--- src/App.js
+++ src/App.js
@@ -1,83 +1,101 @@
 import React, { useState, useEffect } from "react";
-import ItemManagerContract from "./contracts/ItemManager.json";
-import ItemContract from "./contracts/Item.json";
-import getWeb3 from "./getWeb3";
+import ItemManagerContract from "./artifacts/contracts/ItemManager.sol/ItemManager.json";
+import ItemContract from "./artifacts/contracts/Item.sol/Item.json";
+import {ethers} from 'ethers';
+
 import "./App.css";
 
 const App = () =>  {
-  const [web3, setWeb3] = useState(null);
-  const [accounts, setAccounts ] = useState(null);
-  const [contract, setContract ] = useState(null);
-  const [storageValue, setStorageValue ] = useState(null);
+  const [currentAccount, setCurrentAccount ] = useState(null);
+  const [itemManagerContract, setItemManagerContract] = useState(null);
+  const [itemContract, setItemContract ] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
-  const runExample = async (contract, accounts) => {
-    // Stores a given value, 5 by default.
-    await contract.methods.createItem("test", 1000).send({ from: accounts[0] });
+  const ITEMMANAGER_CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  const ITEM_CONTRACT_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
+	const checkIfWalletIsConnected = async () => {
+		try {
+			const { ethereum } = window;
 
-    // Update state with the result.
-    setStorageValue(response);
-  };
+			if (!ethereum) {
+				console.log('Make sure you have MetaMask!');
+				setIsLoading(false);
+				return;
+			} else {
+				console.log('We have the ethereum object', ethereum);
+        
+        let chainId = await ethereum.request({ method: 'eth_chainId' });
+        console.log("Connected to chain " + chainId);
+        // String, hex code of the chainId of the Rinkebey test network
+        const rinkebyChainId = "0x4"; 
+        if (chainId !== rinkebyChainId) {
+          alert("You are not connected to the Rinkeby Test Network!");
+          return;
+        }
+
+				const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+				if (accounts.length !== 0) {
+					const account = accounts[0];
+					console.log('Found an authorized account:', account);
+          // toast.success('Connected!')
+					setCurrentAccount(account);
+				} else {
+					console.log('No authorized account found');
+				}
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const connectWalletAction = async () => {
+		try {
+			const { ethereum } = window;
+
+			if (!ethereum) {
+				alert('Get MetaMask!');
+				return;
+			}
+      
+
+			const accounts = await ethereum.request({
+				method: 'eth_requestAccounts'
+			});
+      // toast.success('Connection established!')
+			console.log('Connected', accounts[0]);
+			setCurrentAccount(accounts[0]);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
   useEffect(() => {
-    const initialFetch = async () => {
-      try {
-        // Get network provider and web3 instance.
-        const web3 = await getWeb3();
-  
-        // Use web3 to get the user's accounts.
-        const AccountList = await web3.eth.getAccounts();
-  
-        // Get the contract instance.
-        const networkId = await web3.eth.net.getId();
-        const deployedNetwork = ItemManagerContract.networks[networkId];
-        const instance = new web3.eth.Contract(
-          ItemManagerContract.abi,
-          deployedNetwork && deployedNetwork.address,
-        );
-        
-        // Set web3, accounts, and contract to the state, and then proceed with an
-        // example of interacting with the contract's methods.
-        setWeb3(web3);
-        setAccounts(AccountList);
-        setContract(instance);
-        runExample(instance, AccountList);
-      } catch (error) {
-        // Catch any errors for any of the above operations.
-        alert(
-          `Failed to load web3, accounts, or contract. Check console for details.`,
-        );
-        console.error(error);
-      }
-    };
-    initialFetch();
-  }, [])
+		checkIfWalletIsConnected();
+		setIsLoading(false);
+	}, []);
+
+  useEffect(() => {
+		const { ethereum } = window;
+
+		if (ethereum) {
+			const provider = new ethers.providers.Web3Provider(ethereum);
+			const signer = provider.getSigner();
+			const itemManagerContractEther = new ethers.Contract(ITEMMANAGER_CONTRACT_ADDRESS, ItemManagerContract.abi, signer);
+			setItemManagerContract(itemManagerContractEther);
+
+      const itemContractEther = new ethers.Contract(ITEM_CONTRACT_ADDRESS, ItemContract.abi, signer);
+			setItemContract(itemContractEther);
+		} else {
+			console.log('Ethereum object not found');
+		}
+	}, []);
 
 
   return (
     <>
-      {
-        (web3 === null) ? 
-        <div>Loading Web3, accounts, and contract...</div>
-        :
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
-        <div>The stored value is: {storageValue}</div>
-      </div>
-
-      }
+      <h2>hello</h2>
     </>
   );
 }
